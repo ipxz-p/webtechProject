e1d3a8c25705b2f4f0496a1c7061888f74aa8922
Pong Siri <64070193@it.kmitl.ac.th>
2022-10-28 23:35:01 +0700

    :seedling: add search page

--- src/services/connection.php
+++ src/services/connection.php
@@ -1,5 +1,7 @@
 <?php
-    
+    $host = "localhost";
+    $dbname = "test2";
+    $user = "root";
     try {
         $con = new PDO("mysql:host=$host;dbname=$dbname", $user, '');
     } catch (PDOException $e) {
